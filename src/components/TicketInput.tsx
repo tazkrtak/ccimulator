3560943846759c5d1462024e2489257e60cd739c
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-04-13 19:56:49 +0200

    support text input for tickets

--- src/components/TicketInput.tsx
+++ src/components/TicketInput.tsx
@@ -14,34 +14,47 @@ export const TicketInput: React.FC<TicketInputProps> = ({
   onChange,
   onError,
 }) => {
-  const [src, setSrc] = useState<string>();
+  const [value, setValue] = useState<string>();
 
   useEffect(() => {
-    document.onpaste = async (event) => {
-      var item = event.clipboardData?.items[0];
-      if (item == null) return;
-
-      if (item.type.indexOf('image') !== 0) {
-        onError('Unsupported format');
-      }
-
-      const blob = item.getAsFile();
-
-      if (blob) {
-        try {
-          const src = window.URL.createObjectURL(blob);
-          setSrc(src);
-
-          const reader = new BrowserQRCodeReader();
-          const qrData = await reader.decodeFromImageUrl(src);
-          const ticket = decodeTicket(qrData.text);
-          onChange(ticket);
-        } catch {
-          onError('Not a QR code image');
-        }
-      }
-    };
+    document.onpaste = handleOnPaste;
   });
+
+  const handleOnPaste = (event: ClipboardEvent) => {
+    var item = event.clipboardData?.items[0];
+
+    const blob = item?.getAsFile();
+    if (blob) handleOnImagePaste(blob);
+    else item?.getAsString(handleOnTextPaste);
+  };
+
+  const handleOnImagePaste = async (blob: File) => {
+    try {
+      const src = window.URL.createObjectURL(blob);
+      const reader = new BrowserQRCodeReader();
+      const qrData = await reader.decodeFromImageUrl(src);
+
+      const ticket = decodeTicket(qrData.text, onError);
+      if (ticket) {
+        setValue(src);
+        onChange(ticket);
+      }
+    } catch {
+      onError('Not a QR code image');
+    }
+  };
+
+  const handleOnTextPaste = (text: string) => {
+    try {
+      const ticket = decodeTicket(text, onError);
+      if (ticket) {
+        setValue(text);
+        onChange(ticket);
+      }
+    } catch (e) {
+      onError('Something wrong or unexpected happened');
+    }
+  };
 
   return (
     <Text
@@ -53,12 +66,16 @@ export const TicketInput: React.FC<TicketInputProps> = ({
         alignItems: 'center',
         justifyContent: 'center',
         margin: 0,
+        textAlign: 'center',
+        color: !value ? 'darkgray' : 'black',
       }}
     >
-      {!src ? (
-        'Paste QR Code Here'
+      {!value ? (
+        "Paste ticket's text or the QR code image here"
+      ) : value.startsWith('blob:') ? (
+        <Image src={value} width={size} height={size} />
       ) : (
-        <Image src={src} width={size} height={size} />
+        value
       )}
     </Text>
   );
--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -5,7 +5,19 @@ export interface Ticket {
   price: number;
 }
 
-export const decodeTicket = (code: string): Ticket => {
+export const decodeTicket = (
+  code: string,
+  onError: (err: string) => void,
+): Ticket | null => {
+  if (!code.match(/.+:\d{6}:\d+:\d+/g)) {
+    onError(
+      'Unsupported ticket format!\n' +
+        'Expected Format: <UserId>:<TOTP>:<Quantity>:<Price>',
+    );
+
+    return null;
+  }
+
   const values = code.split(':');
   return {
     userId: values[0],
