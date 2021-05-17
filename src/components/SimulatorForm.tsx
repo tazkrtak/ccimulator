675eb46f5019e4b0e6ff3d6830b39c2b303f1ff2
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-05-17 22:54:50 +0200

    style: make layout responsive

--- src/components/SimulatorForm.tsx
+++ src/components/SimulatorForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Col, Row, Spacer, Table, useToasts } from '@geist-ui/react';
+import { Button, Grid, Spacer, Table, useToasts } from '@geist-ui/react';
 import axios from 'axios';
 import { useMutation } from 'react-query';
 import { Ticket } from '../models/Ticket';
@@ -30,14 +30,8 @@ export const SimulatorForm: React.FC = () => {
   );
 
   return (
-    <Row gap={3} justify="center" style={{ padding: 32 }}>
-      <Col
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+    <Grid.Container gap={4}>
+      <Grid xs={24} md={12} direction="column" alignItems="center">
         <TicketInput
           size={250}
           onChange={(t) => setTicket(t)}
@@ -57,10 +51,11 @@ export const SimulatorForm: React.FC = () => {
         >
           Purchase
         </Button>
-      </Col>
+      </Grid>
 
-      <Col>
+      <Grid xs={24} md={12}>
         <Table
+          style={{ wordBreak: 'break-all' }}
           data={[
             { prop: 'User Id', value: ticket?.userId },
             { prop: 'User Key', value: ticket?.userKey },
@@ -69,12 +64,10 @@ export const SimulatorForm: React.FC = () => {
             { prop: 'Price', value: ticket?.price },
           ]}
         >
-          <Table.Column prop="prop" label="property" />
+          <Table.Column prop="prop" label="property" width={96} />
           <Table.Column prop="value" label="value" />
         </Table>
-
-        <Spacer y={1} />
-      </Col>
-    </Row>
+      </Grid>
+    </Grid.Container>
   );
 };
